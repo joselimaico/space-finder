b8c0ce8ab8769c78103d15ffc2f680c768ae7aba
limaico <francisco.limaico@kushkipagos.com>
2024-01-13 17:18:07 -0500

    add testing local and spaces

--- src/infra/stacks/ApiStack.ts
+++ src/infra/stacks/ApiStack.ts
@@ -3,16 +3,17 @@ import { LambdaIntegration, RestApi } from "aws-cdk-lib/aws-apigateway";
 import { Construct } from "constructs";
 
 interface ApiStackProps extends StackProps {
-  helloLambdaIntegration: LambdaIntegration;
+  spacesLambdaIntegration: LambdaIntegration;
 }
 
 export class ApiStack extends Stack {
   constructor(scope: Construct, id: string, props: ApiStackProps) {
     super(scope, id, props);
-    const { helloLambdaIntegration } = props;
+    const { spacesLambdaIntegration } = props;
 
     const api = new RestApi(this, "SpacesApi");
     const spacesResource = api.root.addResource("spaces");
-    spacesResource.addMethod("GET", helloLambdaIntegration);
+    spacesResource.addMethod("GET", spacesLambdaIntegration);
+    spacesResource.addMethod("POST", spacesLambdaIntegration);
   }
 }
